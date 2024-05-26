38072457e747f4815f01347616a50c33cb944b3c
CoelhoVin <coelhoviniciusp@gmail.com>
2024-05-26 15:36:45 -0300

    first commit

---
+++ src/components/Post.tsx
@@ -0,0 +1,131 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
+
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+export interface PostType {
+    id: number;
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+interface PostProps {
+    post: PostType;
+}
+
+
+
+export function Post({ post }: PostProps){
+
+    const [comments, setComments] = useState(["Post muito bacana, Hein?!"])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
+
+    const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBr})
+    const publishedDateRelativetoNow = formatDistanceToNow(post.publishedAt, {locale: ptBr, addSuffix: true} )
+
+
+    function handleCreateNewComment(event: FormEvent) {
+        event.preventDefault();
+        // const newCommentText = event.target.comment.value;
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value);
+    }
+
+    function deleteComment(commentToDelete: string) {
+        // IMUTABILIDADE -> CRIAR UMA NOVA INFORMAÇÃO E SALVANDO NO ESTADO useStade, não altera de fato os valor de uma variavel
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+        // console.log(event);
+        event.target.setCustomValidity("Este campo é obrigatório!");
+    }
+
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={post.author.avatarUrl}/>
+                    <div className={styles.authorInfo}>
+                        <strong>{post.author.name}</strong>
+                        <span>{post.author.role}</span>
+                    </div>
+                </div>
+
+                <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>{publishedDateRelativetoNow} </time>
+            </header>
+
+            <div className={styles.content}>
+                {post.content.map(line => {
+                    if (line.type === 'paragraph'){
+                        return <p key={line.content} >{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                })}
+
+            </div>
+
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+                <strong>Deixe seu comentário</strong>
+                <textarea
+                 name='comment'
+                 placeholder='Deixe um comentário'
+                 value={newCommentText}
+                 onChange={handleNewCommentChange}
+                 onInvalid={handleNewCommentInvalid}
+                 required
+                />
+            
+
+                <footer>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+
+                {comments.map(comment => {
+                    return (
+                        <Comment
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
+                })}
+
+            </div>
+
+        </article>
+    );
+}
